48550661028547455b8e49d941ac556497eca6eb
Robert Böhnke <robb@robb.is>
2014-07-27 13:18:10 +0200

    Create MTLManagedObjectAdapter repository

---
+++ MTLManagedObjectAdapter/MTLManagedObjectAdapter.h
@@ -0,0 +1,13 @@
+//
+//  MTLManagedObjectAdapter.h
+//  MTLManagedObjectAdapter
+//
+//  Created by Robert Böhnke on 27/07/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTLManagedObjectAdapter : NSObject
+
+@end
---
+++ MTLManagedObjectAdapter/MTLManagedObjectAdapter.m
@@ -0,0 +1,13 @@
+//
+//  MTLManagedObjectAdapter.m
+//  MTLManagedObjectAdapter
+//
+//  Created by Robert Böhnke on 27/07/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import "MTLManagedObjectAdapter.h"
+
+@implementation MTLManagedObjectAdapter
+
+@end
